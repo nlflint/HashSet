91da37f4ef10a876eee0eedf0630514af9d70328
Nathan Flint <nlflint@gmail.com>
2019-07-02 21:42:25 -0700

    Created some types

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
 fn main() {
     println!("Hello, world!");
 }
 
-#[test]
-fn it_works() {
+// struct Node<T: Hash + Eq> {
+//     next: Option<Box<Node<T>>>,
+//     value: T
+// }
+
+struct HashSet<T: Hash + Eq + Clone>{
+    count: usize,
+    values: Vec<Option<T>>
 }
 
-#[test]
-fn it_fails() {
-    assert!(false);
+fn new_hashset<T: Hash + Eq + Clone>() {
+    HashSet::<T> {
+        count: 0,
+        values: vec![Option::None; 16]
+    };
 }
+
+
+
+#[test]
+fn make_hashset() {
+    let hash_set = new_hashset::<String>();
+    
+}
+
