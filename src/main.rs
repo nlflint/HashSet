6b4973c06f12bb6f1ffed027a409dce0c66c1877
Nathan Flint <nlflint@gmail.com>
2019-07-02 22:36:31 -0700

    Made a constructor

--- src/main.rs
+++ src/main.rs
@@ -9,32 +9,35 @@ trait HashableValue: Hash + Eq + Clone {}
 impl<T> HashableValue for T where T: Hash + Eq + Clone {}
 
 #[derive(Debug, Clone)]
-struct Node<T: Hash + Eq + Clone> {
+struct Node<T: HashableValue> {
     next: Option<Box<Node<T>>>,
     value: T
 }
 
-struct HashSetData<T: Hash + Eq + Clone>{
+struct HashSet<T: HashableValue>{
     count: usize,
     values: Vec<Option<Node<T>>>
 }
 
-trait HashSet<T: HashableValue> { 
-    
+
+impl<T> HashSet<T> where T: HashableValue {
+    fn new() -> Self {
+        HashSet::<T> {
+            count: 0,
+            values: vec![Option::None; 16]
+        }
+        
+    }
 }
 
-impl<T> HashSet<T> for HashSetData<T> where T: HashableValue {}
-
-fn new_hashset<T: Hash + Eq + Clone>() {
-    HashSetData::<T> {
-        count: 0,
-        values: vec![Option::None; 16]
-    };
+fn new_hashset<T: HashableValue>() {
+    HashSet::<T>::new();
 }
 
 #[test]
 fn make_hashset() {
     let hash_set = new_hashset::<String>();
+    //hash_set.add("yo".to_string())
     
 }
 
